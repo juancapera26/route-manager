fe8d015309664a24eea9e75b576825abd6d72f2a
juancapera26 <caperajuandavid26@gmail.com>
2025-09-01 13:08:37 -0500

    fix: corrige errores de compilación y actualiza configuración de TypeScript

---
+++ src/pages/driver/ModalRutas.d.ts
@@ -0,0 +1,10 @@
+import React from "react";
+interface ModalRutasProps {
+    isOpen: boolean;
+    onClose: () => void;
+    isExpanded: boolean;
+    isHovered: boolean;
+    isMobileOpen: boolean;
+}
+declare const ModalRutas: React.FC<ModalRutasProps>;
+export default ModalRutas;
