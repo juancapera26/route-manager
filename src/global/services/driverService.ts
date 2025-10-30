b8a635254e43ed65b33ff6dd4882af8f22ef1299
Jair Duarte <djduarterojas2212@gmail.com>
2025-10-29 23:36:13 -0500

    Traer nuevos cambios

--- src/global/services/driverService.ts
+++ src/global/services/driverService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { UpdateConductorDto } from "../types/conductores";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
 
 export interface ConductorUpdated {
   id: number;
