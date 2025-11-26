eaff4c3ee632eba3bb9667ed4bb2d941441cbd41
juancapera26 <caperajuandavid26@gmail.com>
2025-11-26 03:47:55 -0500

    àctualizacion

--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,4 @@ const isLocalhost = window.location.hostname === "localhost";
 // URL del backend
 export const API_URL = isLocalhost
   ? "http://localhost:8080"
-  : "https://router-manager-backend-456769918123.us-central1.run.app";
+  : "https://router-manager-bakend.onrender.com";
